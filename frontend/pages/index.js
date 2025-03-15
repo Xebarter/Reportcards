aa6df1d03e1b771f808f3a75346a2fd816e1ea3d
Xebarter <sebenock027@gmail.com>
2025-03-15 15:58:23 +0300

    Added pages directory and default homepage

---
+++ frontend/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div className="p-4 bg-gray-100">
+        <h1 className="text-xl font-bold">Welcome to the Report Card System</h1>
+        <p>This is the homepage of the application.</p>
+      </div>
+    );
+  }
---
+++ frontend/pages/test.js
@@ -0,0 +1,8 @@
+export default function TestPage() {
+    return (
+      <div className="p-4 bg-gray-100">
+        <h1 className="text-xl font-bold">Test Page</h1>
+        <p>If you can see this, everything is working!</p>
+      </div>
+    );
+  }
